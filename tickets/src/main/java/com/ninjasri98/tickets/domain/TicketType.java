d74eae5d3feea9478b6e37757032bd446a722c2f
Ninjasri98 <palitsriparno@gmail.com>
2025-07-27 19:25:01 +0530

    generated hashcodes and equals for ticket type

--- tickets/src/main/java/com/ninjasri98/tickets/domain/TicketType.java
+++ tickets/src/main/java/com/ninjasri98/tickets/domain/TicketType.java
@@ -45,6 +45,61 @@ public class TicketType {
     @Column(name = "price", nullable = false)
     private Double price;
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((price == null) ? 0 : price.hashCode());
+        result = prime * result + ((totalAvailable == null) ? 0 : totalAvailable.hashCode());
+        result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+        result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TicketType other = (TicketType) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (price == null) {
+            if (other.price != null)
+                return false;
+        } else if (!price.equals(other.price))
+            return false;
+        if (totalAvailable == null) {
+            if (other.totalAvailable != null)
+                return false;
+        } else if (!totalAvailable.equals(other.totalAvailable))
+            return false;
+        if (createdAt == null) {
+            if (other.createdAt != null)
+                return false;
+        } else if (!createdAt.equals(other.createdAt))
+            return false;
+        if (updatedAt == null) {
+            if (other.updatedAt != null)
+                return false;
+        } else if (!updatedAt.equals(other.updatedAt))
+            return false;
+        return true;
+    }
+
     @Column(name = "total_available")
     private Integer totalAvailable;
 
