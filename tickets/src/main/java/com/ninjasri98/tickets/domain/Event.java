fdbf459ec92dc8f1b2770fcb67c600665826fb63
Ninjasri98 <palitsriparno@gmail.com>
2025-07-27 19:20:33 +0530

    generated hashcode and equals for events

--- tickets/src/main/java/com/ninjasri98/tickets/domain/Event.java
+++ tickets/src/main/java/com/ninjasri98/tickets/domain/Event.java
@@ -69,6 +69,82 @@ public class Event {
     @JoinColumn(name = "organizer_id")
     private User organizer;
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((start == null) ? 0 : start.hashCode());
+        result = prime * result + ((end == null) ? 0 : end.hashCode());
+        result = prime * result + ((venue == null) ? 0 : venue.hashCode());
+        result = prime * result + ((salesStart == null) ? 0 : salesStart.hashCode());
+        result = prime * result + ((salesEnd == null) ? 0 : salesEnd.hashCode());
+        result = prime * result + ((status == null) ? 0 : status.hashCode());
+        result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+        result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Event other = (Event) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (start == null) {
+            if (other.start != null)
+                return false;
+        } else if (!start.equals(other.start))
+            return false;
+        if (end == null) {
+            if (other.end != null)
+                return false;
+        } else if (!end.equals(other.end))
+            return false;
+        if (venue == null) {
+            if (other.venue != null)
+                return false;
+        } else if (!venue.equals(other.venue))
+            return false;
+        if (salesStart == null) {
+            if (other.salesStart != null)
+                return false;
+        } else if (!salesStart.equals(other.salesStart))
+            return false;
+        if (salesEnd == null) {
+            if (other.salesEnd != null)
+                return false;
+        } else if (!salesEnd.equals(other.salesEnd))
+            return false;
+        if (status != other.status)
+            return false;
+        if (createdAt == null) {
+            if (other.createdAt != null)
+                return false;
+        } else if (!createdAt.equals(other.createdAt))
+            return false;
+        if (updatedAt == null) {
+            if (other.updatedAt != null)
+                return false;
+        } else if (!updatedAt.equals(other.updatedAt))
+            return false;
+        return true;
+    }
+
     @ManyToMany(mappedBy = "attendingEvents")
     @Builder.Default
     private List<User> attendees = new ArrayList<>();
