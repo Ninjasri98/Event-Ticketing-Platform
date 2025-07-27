f4959b591b1fa1191567071b5a3ff224b536bcb0
Ninjasri98 <palitsriparno@gmail.com>
2025-07-27 19:51:13 +0530

    started with user repository

--- tickets/src/main/java/com/ninjasri98/tickets/domain/User.java
+++ tickets/src/main/java/com/ninjasri98/tickets/domain/User.java
@@ -18,7 +18,6 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,7 +28,6 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class User {
 
     @Id
@@ -43,7 +41,6 @@ public class User {
     private String email;
 
     @OneToMany(mappedBy = "organizer", cascade = CascadeType.ALL)
-    @Builder.Default
     private List<Event> organizedEvents = new ArrayList<>();
 
     @ManyToMany
@@ -52,7 +49,6 @@ public class User {
         joinColumns = @JoinColumn(name = "user_id"),
         inverseJoinColumns = @JoinColumn(name = "event_id")
     )
-    @Builder.Default
     private List<Event> attendingEvents = new ArrayList<>();
 
     @Override
@@ -110,7 +106,6 @@ public class User {
         joinColumns = @JoinColumn(name = "user_id"),
         inverseJoinColumns = @JoinColumn(name = "event_id")
     )
-    @Builder.Default
     private List<Event> staffingEvents = new ArrayList<>();
 
     @CreatedDate
---
+++ tickets/src/main/java/com/ninjasri98/tickets/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ninjasri98.tickets.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ninjasri98.tickets.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
