12fbc8066e003f319cd269b6221b0d547e13c668
Ninjasri98 <palitsriparno@gmail.com>
2025-07-30 19:48:46 +0530

    added response dtos and mappers for event creation

---
+++ tickets/src/main/java/com/ninjasri98/tickets/mappers/EventMapper.java
@@ -0,0 +1,18 @@
+package com.ninjasri98.tickets.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import com.ninjasri98.tickets.domain.CreateEventRequest;
+import com.ninjasri98.tickets.domain.CreateTicketTypeRequest;
+import com.ninjasri98.tickets.domain.dtos.CreateEventRequestDto;
+import com.ninjasri98.tickets.domain.dtos.CreateEventResponseDto;
+import com.ninjasri98.tickets.domain.dtos.CreateTicketTypeRequestDto;
+import com.ninjasri98.tickets.domain.entities.Event;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+ public interface EventMapper {
+   CreateTicketTypeRequest fromDto(CreateTicketTypeRequestDto dto);
+   CreateEventRequest fromDto(CreateEventRequestDto dto);
+   CreateEventResponseDto toDto(Event event);
+ }
