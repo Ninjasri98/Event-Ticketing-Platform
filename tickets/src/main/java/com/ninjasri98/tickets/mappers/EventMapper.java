00f93d0ecf537c917147f7fca49c4d8250d98a81
Ninjasri98 <palitsriparno@gmail.com>
2025-08-01 19:02:50 +0530

    added mapping and controlling

--- tickets/src/main/java/com/ninjasri98/tickets/mappers/EventMapper.java
+++ tickets/src/main/java/com/ninjasri98/tickets/mappers/EventMapper.java
@@ -8,11 +8,20 @@ import com.ninjasri98.tickets.domain.CreateTicketTypeRequest;
 import com.ninjasri98.tickets.domain.dtos.CreateEventRequestDto;
 import com.ninjasri98.tickets.domain.dtos.CreateEventResponseDto;
 import com.ninjasri98.tickets.domain.dtos.CreateTicketTypeRequestDto;
+import com.ninjasri98.tickets.domain.dtos.ListEventResponseDto;
+import com.ninjasri98.tickets.domain.dtos.ListEventTicketTypeResponseDto;
 import com.ninjasri98.tickets.domain.entities.Event;
+import com.ninjasri98.tickets.domain.entities.TicketType;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
- public interface EventMapper {
-   CreateTicketTypeRequest fromDto(CreateTicketTypeRequestDto dto);
-   CreateEventRequest fromDto(CreateEventRequestDto dto);
-   CreateEventResponseDto toDto(Event event);
- }
+public interface EventMapper {
+  CreateTicketTypeRequest fromDto(CreateTicketTypeRequestDto dto);
+
+  CreateEventRequest fromDto(CreateEventRequestDto dto);
+
+  CreateEventResponseDto toDto(Event event);
+
+  ListEventTicketTypeResponseDto toDto(TicketType ticketType);
+
+  ListEventResponseDto toListEventResponseDto(Event event);
+}
