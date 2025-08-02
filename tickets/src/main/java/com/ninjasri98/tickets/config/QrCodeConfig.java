769ee4f0a791d9961f0fb1252576390600c3ae54
Ninjasri98 <palitsriparno@gmail.com>
2025-08-02 18:19:01 +0530

    implemented QR code generation

---
+++ tickets/src/main/java/com/ninjasri98/tickets/config/QrCodeConfig.java
@@ -0,0 +1,14 @@
+package com.ninjasri98.tickets.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.zxing.qrcode.QRCodeWriter;
+
+@Configuration
+public class QrCodeConfig {
+    @Bean
+    public QRCodeWriter qrCodeWriter() {
+        return new QRCodeWriter();
+    }
+}
